c85204abd3c5e048a789bfa4ddb4a61987b84e33
Anna <annapn5@mail.ru>
2022-12-24 22:16:41 +0600

    решила 3 задачу к третьему уроку

---
+++ program3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+int[] FillArrey()
+{
+    Console.WriteLine("Введите количество целых значений массива");
+    String nu = Console.ReadLine();
+    int N = Convert.ToInt32(nu);
+    int m = 1;
+    int[] Array = Enumerable.Range(m, N-m+1).ToArray(); //создать последовательность
+    Console.WriteLine(String.Join(",", Array));
+    return Array;
+}    
+ 
+ void  KubedNum(int[] Array)  
+ {
+    int res = 0;
+    int index = 0;
+    int len = Array.Length;
+    while(index < len)
+    {
+        int number= Array[index];
+        res = number*number*number;
+        Console.WriteLine(res);
+        index++;   
+    }
+ }
+int[] Array = FillArrey();
+KubedNum(Array);
